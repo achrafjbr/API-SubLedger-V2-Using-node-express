2464a670f59ddb0438382e2581b9bb8755579b94
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-03-17 09:56:48 +0100

    Adding Transaction schema

---
+++ src/models/Transaction.js
@@ -0,0 +1,20 @@
+
+const mongoose = require("mongoose");
+
+const subscriptionSchema = mongoose.Schema({
+  paymentDate: {
+    type: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+
+}, { timestamps: true });
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+module.exports = {
+  Subscription
+}
