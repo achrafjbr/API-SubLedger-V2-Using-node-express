16136fe1dd56d1d96a0b1dc78d7f8f761f301812
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-03-17 13:40:45 +0000

    Set up the admin router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const connectDb = require('./src/config/db');
-const { PREFIX_ROUTE_PATH } = require('./src/utils/constants')
-require('dotenv').config();
+const express = require("express");
+const connectDb = require("./src/config/db");
+const { PREFIX_ROUTE_PATH } = require("./src/utils/constants");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,16 +10,25 @@ app.use(express.json());
 //Connection establishement
 connectDb();
 
-
 //Routes...
-app.use(`${PREFIX_ROUTE_PATH}/auth`, require('./src/routes/authenticationRouter')
-.authenticationRouter)
-app.use(`${PREFIX_ROUTE_PATH}/subscription`, require('./src/routes/subscriptionsRouter')
-.subscriptionRouter);
-app.use(`${PREFIX_ROUTE_PATH}/users`, require('./src/routes/userRouter').userRouter);
-
+app.use(
+    `${PREFIX_ROUTE_PATH}/auth`,
+    require("./src/routes/authenticationRouter").authenticationRouter
+);
+app.use(
+    `${PREFIX_ROUTE_PATH}/subscription`,
+    require("./src/routes/subscriptionsRouter").subscriptionRouter
+);
+app.use(
+    `${PREFIX_ROUTE_PATH}/users`,
+    require("./src/routes/userRouter").userRouter
+);
+app.use(
+    `${PREFIX_ROUTE_PATH}/admin`,
+    require("./src/routes/adminRouter").adminRouter
+);
 
 // Server.
 app.listen(process.env.PORT || 3000, (err) => {
-  if (!err) console.log("Server Starting...");
+    if (!err) console.log("Server Starting...");
 });
